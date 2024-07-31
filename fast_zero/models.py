17ac15ff73d08f148afc560f4098930c960af586
Victor Contreras <contreras3991@gmail.com>
2024-07-31 13:58:54 -0300

    added updated_at column into User model

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -15,5 +15,11 @@ class User:
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     create_at: Mapped[datetime] = mapped_column(
-        init=False, server_default=func.now()
+        init=False,
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+        onupdate=func.now(),
     )
