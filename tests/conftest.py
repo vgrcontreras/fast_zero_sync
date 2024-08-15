52a7e5a00c9dfcbe7eb5f90196301859046bf442
Victor Contreras <contreras3991@gmail.com>
2024-08-15 15:20:44 -0300

    refactored other_user fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,15 +63,11 @@ def user(session):
 
 @pytest.fixture
 def other_user(session):
-    pwd = 'testtest'
-
-    user = UserFactory(password=get_password_hash(pwd))
+    user = UserFactory()
 
     session.add(user)
     session.commit()
     session.refresh(user)
-
-    user.clean_password = pwd
 
     return user
 
