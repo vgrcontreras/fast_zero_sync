b1b0834cbd805ee82ede184b708f3a4e6654ffd5
Victor Contreras <contreras3991@gmail.com>
2024-08-15 15:19:32 -0300

    added except ExpiredSignatureError in get_current_user function

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jwt import decode, encode
-from jwt.exceptions import PyJWTError
+from jwt.exceptions import ExpiredSignatureError, PyJWTError
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -59,6 +59,8 @@ def get_current_user(
 
         if not email:
             raise credentials_exception
+    except ExpiredSignatureError:
+        raise credentials_exception
     except PyJWTError:
         raise credentials_exception
 
