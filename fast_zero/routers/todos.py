6ca0d109f7be94e34dc65dfa387b7cf38dc63e4a
Victor Contreras <contreras3991@gmail.com>
2024-08-15 22:55:41 -0300

    created creat and list todos endpoints

---
+++ fast_zero/routers/todos.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from fast_zero.database import get_session
+from fast_zero.models import ToDo
+from fast_zero.schemas import ToDoPublic, ToDoSchema, ToDoList, ToDoState
+from fast_zero.security import get_current_user
+
+router = APIRouter(prefix='/todos', tags=['todos'])
+
+
+@router.post('/', response_model=ToDoPublic)
+def create_to_do(
+    todo: ToDoSchema,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    db_todo = ToDo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=current_user.id,
+    )
+
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
+
+@router.get('/', response_model=ToDoList)
+def list_todos(
+    session: Session = Depends(get_session),
+    current_user = Depends(get_current_user),
+    title: str | None = None,
+    description: str | None = None,
+    state: ToDoState | None = None,
+    offset: int = None,
+    limit: int = None,
+):
+    query = select(ToDo).where(ToDo.user_id == current_user.id)
+
+    if title:
+        query = query.where(ToDo.title.contains(title))
+
+    if description:
+        query = query.where(ToDo.description.contains(description))
+    
+    if state:
+        query = query.where(ToDo.state == state)
+    
+    todos = session.scalar(select(ToDo).limit(limit).offset(offset))
+
+    return {'todos': todos}
