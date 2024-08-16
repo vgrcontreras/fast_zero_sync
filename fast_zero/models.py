7188a1d0562068c2d3e5b42d805c42ef0a2624a4
Victor Contreras <contreras3991@gmail.com>
2024-08-15 22:54:35 -0300

    created ToDoState class and todos table dataclass

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,9 +1,18 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
+
+
+class ToDoState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
 
 
 @table_registry.mapped_as_dataclass
@@ -22,3 +31,14 @@ class User:
         server_default=func.now(),
         onupdate=func.now(),
     )
+
+
+@table_registry.mapped_as_dataclass
+class ToDo:
+    __tablename__ = 'todos'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[ToDoState]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
