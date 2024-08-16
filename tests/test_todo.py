9cee68b2cab926a58afa93ad6b0631bc3996bc5d
Victor Contreras <contreras3991@gmail.com>
2024-08-15 22:57:16 -0300

    created test_create_todo for post create_todo endpoint

---
+++ tests/test_todo.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+
+def test_create_todo(client, token, user):
+    response = client.post(
+        '/todos/',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'title': 'Test todo',
+            'description': 'Test todo description',
+            'state': 'draft',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'title': 'Test todo',
+        'description': 'Test todo description',
+        'state': 'draft',
+    }
