04767ca3878e40b0f08341520840086e2b5199c6
Victor Contreras <contreras3991@gmail.com>
2024-08-19 21:16:55 -0300

    updated ToDo class with created_at and updated_at attributes

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -42,3 +42,12 @@ class ToDo:
     description: Mapped[str]
     state: Mapped[ToDoState]
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    created_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
