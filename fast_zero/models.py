f774534f91e08a2ad5984eb96f0dc43be1b1af1c
Victor Contreras <contreras3991@gmail.com>
2024-08-20 23:04:37 -0300

    fixed: added server_default parameter in created_at attr

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -43,8 +43,7 @@ class ToDo:
     state: Mapped[ToDoState]
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     created_at: Mapped[datetime] = mapped_column(
-        init=False,
-        server_default=func.now()
+        init=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
         init=False,
